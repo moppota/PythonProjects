5b48fab048dfe5cfff78ca07e6f92ff165c37330
moppota <49787959+moppota@users.noreply.github.com>
2023-10-26 18:17:05 +0100

    Learning pygame

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-import deeznuts
-import random
-class Plane:
-    def __init__(self, type, age):
-        self.type = type
-        self.age = age
-    def __str__(self):
-        return f"{self.type}({self.age})"
+#following tutorial trying to learn pygame
+import pygame 
 
-A320 = Plane("Airbus A320",0)
+from pygame.locals import (
 
-print(A320)
+    K_UP, # initialising key inputs I need
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+
+)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.surface((75,25))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+running = True
+
+screen.fill((255,255,255))
+
+surf = pygame.Surface((50,50))
+
+surf.fill((0,0,0))
+
+rect = surf.get_rect()
+
+surf_centre = (
+    (SCREEN_WIDTH-surf.get_width())/2,
+    (SCREEN_HEIGHT-surf.get_width())/2
+)
+
+screen.blit(surf, surf_centre)   
+pygame.display.flip()
+
+while running:
+
+    for event in pygame.event.get():
+
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        
+        elif event.type == QUIT:
+            running = False
--- main2.py
+++
@@ -1,8 +0,0 @@
-set = {"one","three","two"}
-
-
-if (str(input("Enter what you would like to search for: \n")) in set) == True:
-    print("This is in the set")
-else: 
-    print("oh noooo")
-
