5ceb03cf5b83abd028e1c9a01da515882ffdc308
moppota <49787959+moppota@users.noreply.github.com>
2023-10-28 17:44:45 +0100

    comments for my spaghetti code

--- YahtzeeShenanigans/YahtzeeInPythonModule.py
+++ YahtzeeShenanigans/YahtzeeInPythonModule.py
@@ -54,20 +54,20 @@ class functionsNeeded:
         for v in self.card.values(): # loops through every value in the dictionary, if its in the upper or lower section adds it to those totals and gives a grand total. 
             i += 1
             if i == 16:
-                grandTotalTemp = upperTotalTemp+lowerTotalTemp
-            if i == 15:
+                grandTotalTemp = upperTotalTemp+lowerTotalTemp # on 16th iteration its on the grand total section
+            if i == 15: #15th iteration its on lower total so it skips 
                 continue
-            if i < 7:
+            if i < 7: # less than 7 means we are on the Ones, Twos etc in the upper section
                 upperTotalTemp += v
-            elif  i > 7:
+            elif  i > 7: # more than 7 means lower section (7 is the upper section total so we skip it)
                 lowerTotalTemp += v
             self.card["Total of Lower Section"] = lowerTotalTemp
             self.card["Total of Upper Section"] = upperTotalTemp
-            self.card["Grand Total"] = grandTotalTemp
+            self.card["Grand Total"] = grandTotalTemp # just puts all the calculated values back into the scorecard
     def addValues(self, Type, Score):
         self.card[Type] = Score # updates the score of a specific value 
 
-    def rollDice(self, number): # rolls dice and returns as a list. 
+    def rollDice(self, number): # rolls a specified amount of dice and returns as a list. 
         dice = []
         for i in range(number):
             dice.append(random.randint(1,6))
@@ -88,15 +88,15 @@ class functionsNeeded:
             match score:
                 case "3 of a kind": # checks if any number in the roll is repeated 3 or more times
                     for i in range(6):
-                        if dice.count(i+1) >= 3:
-                            return True, sum(dice)
+                        if dice.count(i+1) >= 3: # dice have values 1-6, Checks if the list contains 3 or more of 1-6 using a for loop. i+1 since starts at 0
+                            return True, sum(dice) # this is simple, 3 of a kind is just sum of all dice, therefore sum of the list
                         else:
                             if i == 5:
                                 return False, 0
                 case "4 of a kind": # checks if any number in the roll is repeated 4 or more times
                     for i in range(6):
-                        if dice.count(i+1) >= 4:
-                            return True, sum(dice)
+                        if dice.count(i+1) >= 4: # works same as 3 of a kind, however checks for 4 or more
+                            return True, sum(dice) # as above
                         else:
                             if i == 5:
                                 return False, 0
@@ -104,14 +104,14 @@ class functionsNeeded:
                     isThreeofakind = False # if both of these end up being true, its a full house
                     isPair = False
                     for i in range(6):
-                        if dice.count(i+1) == 3:
+                        if dice.count(i+1) == 3: # full house needs a 3 of a kind and a pair, uses same code as 3 of a kind to check if there are 3 of a kind in there
                             isThreeofakind = True
                             break
                     for i in range(6):
                         if dice.count(i+1) == 2:
                             isPair = True
                             break
-                    if isThreeofakind and isPair:
+                    if isThreeofakind and isPair: # if both values are true then it is a full house
                         return True, scoreOfCard[score]
                     else:
                         return False, 0
@@ -122,10 +122,10 @@ class functionsNeeded:
                         if i == 0:
                             pass
                         else:
-                            if (dice[i] - 1) == dice[i-1]:
+                            if (dice[i] - 1) == dice[i-1]: # if the current value is 1 more than the last value, add to in a row counter
                                 amountInARow += 1
                             else:
-                                amountInARow = 0
+                                amountInARow = 0 # if not, reset counter
                     if amountInARow >= 4:
                         return True, scoreOfCard[score]
                     else:
@@ -137,18 +137,18 @@ class functionsNeeded:
                         if i == 0:
                             pass
                         else:
-                            if (dice[i] - 1) == dice[i-1]:
+                            if (dice[i] - 1) == dice[i-1]: # checks if current value is 1 more than last value
                                 amountInARow += 1
                             else:
-                                amountInARow = 0
+                                amountInARow = 0 # reset if not
                     if amountInARow >= 5:
                         return True, scoreOfCard[score]
                     else:
                         return False, 0
                 case "Yahtzee": # Since its sorted and at least 5 need to match, the second option in the list would always be the one thats yahtzee. Just check if the count of that is higher than 5
                     newList = dice.sort()
-                    if newList.count(newList[1]) == 5:
-                        return True, scoreOfCard[score]
+                    if newList.count(newList[1]) == 5: 
+                        return True, scoreOfCard[score] 
                     else:
                         return False, 0
                 case "Chance": #its always possible (only once along with everything else), just return value of everything
