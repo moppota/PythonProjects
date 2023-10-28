0ef03d5b769f8af0fd6535d0c50b87fdba807112
moppota <49787959+moppota@users.noreply.github.com>
2023-10-28 12:43:48 +0100

    First version of program, Not tested

--- YahtzeeShenanigans/YahtzeeInPython.py
+++ YahtzeeShenanigans/YahtzeeInPython.py
@@ -4,29 +4,40 @@ import time
 newGame = YahtzeeInPythonModule.functionsNeeded()
 options = YahtzeeInPythonModule.listOfChoices
 running = True # variables setup
+diceRoll = []
+isOptionValid = True
 
 print("Before we start, here are your choices (For what to do with your dice roll),\nand please enter them exactly as shown:")
 print(options)
 print("\n") # telling players this is how I want them to say where the score should go under
 
 while running == True: # main loop
-    input("Press enter to roll dice")
-    diceRoll = newGame.rollDice()
-    print("Here is your roll, Ordered numerically: ")
-    diceRoll.sort()
-    print(diceRoll)
-    print("\n") # when enter is pressed, rolls dice and shows player
-    while True:
+    for i in range(3):
+        input("Press enter to roll dice")
+        diceRoll = newGame.rollDice()
+        print("Here is your roll, Ordered numerically: ")
+        diceRoll.sort()
+        print(diceRoll)
+        print("\n") # when enter is pressed, rolls dice and shows player
+        if input("Would you like to roll again? y/n") == "y":
+            pass
+        else:
+            break
+    while isOptionValid:
         PlayerChoice = input("What would you like to do? (Please enter exactly as requested at the beginning)")
         if (PlayerChoice in options) == True:
-            options.remove(PlayerChoice)
-            print("Success, Added to your score. \n")
-            newGame.refreshTotals()
-            print(newGame.card)
-            print("\n")
-            break # if the option is valid, adds the score to that option and removes it from list since 
-        else:
-            print("Not a valid option. Try again.\n ")
+            checkIfTrue = newGame.checkIfPossibleAndGetValue(diceRoll, PlayerChoice)
+            if checkIfTrue[0] == True:
+                options.remove(PlayerChoice)
+                newGame.addValues(PlayerChoice, checkIfTrue[1])
+                newGame.refreshTotals()
+                print("Success, Added to your score. \n")
+                print(newGame.card)
+                print("\n")
+                isOptionValid = False # if the option is valid, adds the score to that option and removes it from list since 
+            else:
+                print("Not a valid option. Try again.\n ")
+                PlayerChoice = input("What would you like to do? (Please enter exactly as requested at the beginning)")
     if len(options) == 0:
         print("Well done, you have completed the game. Here is your ScoreCard: \n")
         newGame.refreshTotals()
--- YahtzeeShenanigans/YahtzeeInPythonModule.py
+++ YahtzeeShenanigans/YahtzeeInPythonModule.py
@@ -1,12 +1,12 @@
 import random
 CardTemplate = {
 
-    "Ones":1,
-    "Twos":2,
-    "Threes":3,
-    "Fours":4,
-    "Fives":5,
-    "Sixes":6,
+    "Ones":0,
+    "Twos":0,
+    "Threes":0,
+    "Fours":0,
+    "Fives":0,
+    "Sixes":0,
 
     "Total of Upper Section":0,
 
@@ -23,10 +23,26 @@ CardTemplate = {
     "Grand Total":0
 } # defining the score card of Yahtzee as a dictionary, and will show players what their score is
 
+scoreOfCard = {
+    "Ones":1,
+    "Twos":2,
+    "Threes":3,
+    "Fours":4,
+    "Fives":5,
+    "Sixes":6,
+
+    "Full House": 25,
+    "Small Straight": 30,
+    "Large Straight": 40,
+    "Yahtzee": 50,
+} # These options have a set amount of points, so I will just call them from here anmd it allows me to loop stuff
+
 listOfChoices = []
 
 for v in CardTemplate:
     listOfChoices.append(v) #literally cant be assed to type out every option so I spent the same amount of time automating it. At least I can add stuff to the template if needed
+
+
 
 class functionsNeeded:
     def __init__(self):
@@ -57,9 +73,94 @@ class functionsNeeded:
             dice.append(random.randint(1,6))
         return dice
     
-    def checkIfPossible(self, dice, score):
-        if score not in listOfChoices:
-            return False
+    def checkIfPossibleAndGetValue(self, dice, score): # checking if the option given is legal with the dice roll and gives the value
+        x = 0
+        if score not in listOfChoices: # if its not in list of choices just return false, function shouldnt be called if its not in their anyway
+            return False, 0
+        else:
+            for v in scoreOfCard: # checking the ones, twos, threes etc. 
+                x+=1
+                if v == score and x < 7 and dice.count(scoreOfCard[score]) != 1: # loops through the score dictionary until option is reached, check if the roll has any numbers of the specified one in it 
+                    returnValue = dice.count(scoreOfCard[score]) * scoreOfCard[score]
+                    return True, returnValue
+                if x > 7: # above 7 we just stop the loop since this was not one of the chosen options
+                    break
+            match score:
+                case "3 of a kind": # checks if any number in the roll is repeated 3 or more times
+                    for i in range(6):
+                        if dice.count(i+1) >= 3:
+                            return True, sum(dice)
+                        else:
+                            if i == 5:
+                                return False, 0
+                case "4 of a kind": # checks if any number in the roll is repeated 4 or more times
+                    for i in range(6):
+                        if dice.count(i+1) >= 4:
+                            return True, sum(dice)
+                        else:
+                            if i == 5:
+                                return False, 0
+                case "Full House":
+                    isThreeofakind = False # if both of these end up being true, its a full house
+                    isPair = False
+                    for i in range(6):
+                        if dice.count(i+1) == 3:
+                            isThreeofakind = True
+                            break
+                    for i in range(6):
+                        if dice.count(i+1) == 2:
+                            isPair = True
+                            break
+                    if isThreeofakind and isPair:
+                        return True, scoreOfCard[score]
+                    else:
+                        return False, 0
+                case "Small Straight": # loops through list and sees how many are in a row, if 4 or more, its valid
+                    sortedDice = dice.sort()
+                    amountInARow = 0
+                    for i in range(6):
+                        if i == 0:
+                            pass
+                        else:
+                            if (dice[i] - 1) == dice[i-1]:
+                                amountInARow += 1
+                            else:
+                                amountInARow = 0
+                    if amountInARow >= 4:
+                        return True, scoreOfCard[score]
+                    else:
+                        return False, 0
+                case "Large Straight": # same as above but it needs 5 in a row
+                    sortedDice = dice.sort()
+                    amountInARow = 0
+                    for i in range(6):
+                        if i == 0:
+                            pass
+                        else:
+                            if (dice[i] - 1) == dice[i-1]:
+                                amountInARow += 1
+                            else:
+                                amountInARow = 0
+                    if amountInARow >= 5:
+                        return True, scoreOfCard[score]
+                    else:
+                        return False, 0
+                case "Yahtzee": # Since its sorted and at least 5 need to match, the second option in the list would always be the one thats yahtzee. Just check if the count of that is higher than 5
+                    newList = dice.sort()
+                    if newList.count(newList[1]) == 5:
+                        return True, scoreOfCard[score]
+                    else:
+                        return False, 0
+    
+
+
+
+
+                    
+                    
+                        
+
+
         
 
 
